86af91ebeea19873c72e60189c76a7c196ea1872
Wannes Wagemans <58140918+wannes-wagemans@users.noreply.github.com>
2022-02-18 16:39:25 +0100

    commit -m finished redux and features

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -42,16 +42,14 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found")
   }
 
-  const user = await User.findById(req.user.id)
-
   // Check if user excist
-  if (!user) {
+  if (!req.user) {
     res.status(401)
     throw new Error("User not found")
   }
 
   // Logged in user has to match the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401)
     throw new Error("User not authorized")
   }
@@ -73,17 +71,14 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error("Goal not found")
   }
-
-  const user = await User.findById(req.user.id)
-
   // Check if user excist
-  if (!user) {
+  if (!req.user) {
     res.status(401)
     throw new Error("User not found")
   }
 
   // Logged in user has to match the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401)
     throw new Error("User not authorized")
   }
